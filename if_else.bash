fd9b194cf0903b7056801cd987b259e9646e677b
gipeypey <thinkpadgipey@gmail.com>
2022-03-29 21:38:33 +0700

    add if_else.bash

---
+++ if_else.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+name="ghifari"
+if [ "$name" = "gipey" ];
+    then
+        echo "Selamat anda beruntung!"
+    else
+        echo "Jangan menyerah, tetap semangat!"
+fi
+sleep 2
+
+# clear
+# echo "--Google Log In--"
+# read -p "Username: " username
+# if [ "$username" = "gipey" ];
+#     then
+#         echo "Welcome back $username"
+#     else
+#         echo "Please register your account first"
+# fi
+# sleep 2

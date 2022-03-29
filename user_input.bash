16b0b5d213a3d5c034caa7a07023aecc6c73901f
gipeypey <thinkpadgipey@gmail.com>
2022-03-29 21:03:12 +0700

    add user_input.bash

---
+++ user_input.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "--Registration Form--"
+read -p "Enter your name: " name
+read -p "Etner your class: " class
+echo "Your name is $name and your class is $class"
+sleep 2
+
+clear
+echo "--Registration Form--"
+echo "Enter your full name: "
+read fname lname
+echo "Your name is $fname $lname"
+sleep 2
